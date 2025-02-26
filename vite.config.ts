67e8dc392d5622e536e7c51f4d8c57db627cc0e2
incubator4 <aries0robin@gmail.com>
2025-02-26 17:36:47 +0800

    feat: add rollup visualizer and CDN import plugin to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
+import VitePluginCDN from "vite-plugin-cdn-import";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      open: true,
+      emitFile: false,
+      filename: "stats.html",
+      sourcemap: false,
+    }),
+    VitePluginCDN({
+      modules: [
+        {
+          name: "react-scratchcard-v2",
+          var: "ScratchCard",
+          path: "dist/index.min.js",
+        },
+      ],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            if (id.includes("scratchcard")) {
+              return "scratchcard";
+            }
+          }
+          return "vendor";
+        },
+        experimentalMinChunkSize: 10 * 1024,
+      },
+    },
+  },
 });
